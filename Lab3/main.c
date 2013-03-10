c9c4844620205473925cb6b9d6f242bf23b60bbd
Patrick Barrett <Pat@theUnlink.com>
2013-03-10 13:40:30 -0500

    Mostly Working Code
    There is still a strange issue that it won't read or
    write (not sure which) anything but the first value.

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,48 +1,15 @@
 /*
 Participants: Quoc Anh Tran, Patrick Barrett
-IDs:    451 7669, 381 3525
+IDs: 451 7669, 381 3525
 Course: EE4341
-Date:  2/15/2013
+Date: 2/15/2013
 */
+#define MY_I2C
 
 #include <p18f4520.h>
+#ifndef MY_I2C
 #include <i2c.h>
-// PORTD =		1	E	R/W		RS		DB7	DB6	DB5	DB4	
-// RD7 has to be 1 to be powered ON
-
-// LCD 4-bit interface. High nibble transfered first
-// check busy flag before sending an instruction
-
-// LCD 1st line =  0 <-> F, 2nd line = 0x40 -> 0x4F
-
-
-/*
-I/D = 1: Increment
-I/D = 0: Decrement
-S = 1: Accompanies display shift
-S/C = 1:  Display shift
-S/C = 0: Cursor move
-R/L = 1: Shift to the right
-R/L = 0: Shift to the left
-DL = 1: 8 bits, DL = 0:  4 bits
-N = 1: 2 lines, N = 0:  1 line
-F = 1: 5 × 10 dots, F = 0:  5 × 8 dots
-BF = 1: Internally operating
-BF = 0:  Instructions acceptable
-DDRAM: Display data RAM
-CGRAM: Character generator
-RAM
-ACG: CGRAM address
-ADD: DDRAM address (corresponds to cursor address)
-AC: Address counter used for both DD and CGRAM addresses
-
-*/
-/*
-to  correctly  read  data,
-execute either the address set instruction or cursor shift instruction (only with DDRAM), then just
-before  reading  the  desired  data,  execute  the  read  instruction  from  the  second  time  the  read
-instruction is sent.
-*/
+#endif
 
 /* ------ */
 
@@ -57,20 +24,29 @@ instruction is sent.
 
 
 unsigned char cursor = 0; // cursor position
-unsigned int counter = 0;	
-unsigned char data[2] = {0};
-unsigned char digits[2] = {0};
-unsigned char lookUp[10] = {0x30, 0x31, 0x32, 0x33, 0x34,
-							0x35, 0x36, 0x37, 0x38, 0x39};
+unsigned char counter = 0;
+unsigned char counts[4] = {0};
 
-void hi_isr(void); 
-void lo_isr(void); 
+void hi_isr(void);
+void lo_isr(void);
 void setup(void);
 
 
 /* I2C functions */
 void HDByteWriteI2C( unsigned char ControlByte, unsigned char HighAdd, unsigned char LowAdd, unsigned char data);
 void HDByteReadI2C( unsigned char ControlByte, unsigned char HighAdd, unsigned char LowAdd, unsigned char *data, unsigned char length );
+
+#ifdef MY_I2C
+void IdleI2C();
+void StartI2C();
+void RestartI2C();
+void StopI2C();
+void WriteI2C( unsigned char data );
+void ReadI2C();
+void NotAckI2C();
+
+volatile unsigned char I2CIdleFlag = 0;
+#endif
 
 
 /* LCD functions */
@@ -90,7 +66,7 @@ void waitms(unsigned short int ms);
 void hi_isr_entry(void){
     _asm goto hi_isr _endasm
 }
-	
+
 #pragma code lo_isr_entry=0x18
 void lo_isr_entry(void){
     _asm goto lo_isr _endasm
@@ -98,138 +74,133 @@ void lo_isr_entry(void){
 
 #pragma interrupt hi_isr
 void hi_isr(void){
-	
-	HDByteWriteI2C(0xa0, 0, 0, lookUp[digits[0]]);
-	HDByteWriteI2C(0xa0, 0, 1, lookUp[digits[1]]);		
+
+HDByteWriteI2C(0xa0, 0, 0, counter);
 }
 
 #pragma interruptlow lo_isr
 void lo_isr(void){
 
-	static unsigned char interval = 0;
-		
-		PIR1bits.TMR2IF = 0;
+static unsigned char interval = 0;
 
-		if (interval == 19){
-			counter++;
-			if(counter == 100) counter = 0;
-			digits[1] = counter/10;
-			digits[0] = counter-(10*digits[1]);
+PIR1bits.TMR2IF = 0;
 
-			resetCursor();
-			insertChar(lookUp[digits[1]]);
-			insertChar(lookUp[digits[0]]); 
+if (interval == 19){
+counter++;
+if(counter >= 100) counter = 0;
+
+resetCursor();
+insertChar((counter/10) + 0x30);
+insertChar((counter - (counter/10)*10) + 0x30);
 
 
-			interval = 0;
-		} else interval++;	
+interval = 0;
+} else interval++;
 
 }
 
 void enable(void){
-	EN = 1;
-	_asm
-		nop
-		nop
-	_endasm
-	EN = 0;
+EN = 1;
+_asm
+nop
+nop
+_endasm
+EN = 0;
 }
 
 /* initialization based on p.213 in LCD datasheet */
-void initLCD(void){ 
-	ON = 1;
-	waitms(20);
-	PORTD = 0b10000011; // Function set (interface 8-bit)
-	enable();
-	waitms(8);
-	enable();
-	waitms(2);
-	PORTD = 0b10000010; // Function set  (Set interface to be 4 bits long.) Interface is 8 bits in length.
-	enable();
-	PORTD = 0b10000010; // 2 lines, 5x8 dots
-	enable();
-	PORTD = 0b10001000; // 
-	enable();
-	PORTD = 0b10000000; // Display off
-	enable();
-	PORTD = 0b10001000; // 
-	enable();
-	PORTD = 0b10000000; // Display clear
-	enable();
-	PORTD = 0b10000001; // 
-	enable();
-	PORTD = 0b10000000; // Cursor incrementing, no shift
-	enable();
-	PORTD = 0b10000110; // 
-	enable();
-	waitms(3);
-	PORTD = 0b10000000; // Display on, cursor on, blinking cursor
-	enable();
-	PORTD = 0b10001110; // 
-	enable();
-	waitms(5);
+void initLCD(void){
+ON = 1;
+waitms(20);
+PORTD = 0b10000011; // Function set (interface 8-bit)
+enable();
+waitms(8);
+enable();
+waitms(2);
+PORTD = 0b10000010; // Function set (Set interface to be 4 bits long.) Interface is 8 bits in length.
+enable();
+PORTD = 0b10000010; // 2 lines, 5x8 dots
+enable();
+PORTD = 0b10001000; //
+enable();
+PORTD = 0b10000000; // Display off
+enable();
+PORTD = 0b10001000; //
+enable();
+PORTD = 0b10000000; // Display clear
+enable();
+PORTD = 0b10000001; //
+enable();
+PORTD = 0b10000000; // Cursor incrementing, no shift
+enable();
+PORTD = 0b10000110; //
+enable();
+waitms(3);
+PORTD = 0b10000000; // Display on, cursor on, blinking cursor
+enable();
+PORTD = 0b10001110; //
+enable();
+waitms(5);
 }
 
 void insertChar(unsigned char c){
-	unsigned char lowNibble = c & 0b00001111;
+unsigned char lowNibble = c & 0b00001111;
 
-	PORTD = 0b10010000 | (c>>4); // High order nibble
-	enable();
-	PORTD = 0b10010000 | lowNibble; // Low order nibble
-	enable();
-	waitms(5);
+PORTD = 0b10010000 | (c>>4); // High order nibble
+enable();
+PORTD = 0b10010000 | lowNibble; // Low order nibble
+enable();
+waitms(5);
 }
 
 /* determines the position of the cursor */
 void resetCursor (void){
-	PORTD = 0b10000000; 
-	enable();
-	PORTD = 0b10000010; 
-	enable();
-	waitms(5);
+PORTD = 0b10000000;
+enable();
+PORTD = 0b10000010;
+enable();
+waitms(5);
 
 }
 void endLineCheck(void){
-	if (cursor == 0x10){ // end of first line. Cursor jumps to the second line (0x40)
-		PORTD = 0b10001100; 
-		enable();
-		PORTD = 0b10000000; 
-		enable();
-		waitms(5);
-		cursor = 0x41;
-	}
-	else if	(cursor >= 0x50){ // end of second line. Display cleared and cursor jumps to starting position
-		PORTD = 0b10000000; 
-		enable();
-		PORTD = 0b10000001; 
-		enable();
-		waitms(5);
-		cursor = 0x01;
-	}
-	else cursor++;
-	waitms(5);
+if (cursor == 0x10){ // end of first line. Cursor jumps to the second line (0x40)
+PORTD = 0b10001100;
+enable();
+PORTD = 0b10000000;
+enable();
+waitms(5);
+cursor = 0x41;
+}
+else if	(cursor >= 0x50){ // end of second line. Display cleared and cursor jumps to starting position
+PORTD = 0b10000000;
+enable();
+PORTD = 0b10000001;
+enable();
+waitms(5);
+cursor = 0x01;
+}
+else cursor++;
+waitms(5);
 }
 
 // delay 1 ms
 void wait1ms(void)
 {
-	unsigned long int u = 169;
+unsigned long int u = 169;
 
-	while (u--) ;
+while (u--) ;
 }
 
 // delays ms number of milliseconds
 void waitms(unsigned short int ms)
 {
-	while (ms--)
-	{
-		wait1ms();
-	}
+while (ms--)
+{
+wait1ms();
+}
 }
 
 void setup(void){
-	
-	unsigned char tmp;
 	TRISBbits.TRISB3 = 0;
 	PORTBbits.RB3 = 0;
 
@@ -238,7 +209,7 @@ void setup(void){
 	// Set LCD interface
 	TRISD = 0;
 	PORTD = 0;
-	initLCD();	
+	initLCD();
 
 	// Timer2
 	PR2 = 250;
@@ -250,75 +221,118 @@ void setup(void){
 	T2CONbits.TMR2ON = 1;
 
 	// I2C config
-	DDRCbits.RC3 = 1; //Configure SCL as Input
-	DDRCbits.RC4 = 1; //Configure SDA as Input
+	SSPSTAT = 0x80; //Disable SMBus
+	SSPCON1 = 0b00101011; //Enable MSSP Master
+	SSPADD = 0x28; //Should be 0x18
+	SSPCON2 = 0x00; //Clear MSSP Conrol Bits
 
- 	
-	SSPSTAT = 0x80;   //Disable SMBus 
-  	SSPCON1 = 0x28;   //Enable MSSP Master
-  	SSPADD = 0x27;    //Should be 0x18
- 	SSPCON2 = 0x00;   //Clear MSSP Conrol Bits
 
 	// Button settings
 	TRISAbits.TRISA4 = 1;
-	
+
 	// HLVD settings
 	PIR2bits.HLVDIF = 0;
 	IPR2bits.HLVDIP = 1;
 	PIE2bits.HLVDIE = 1;
 	HLVDCON = 0x3E;
-	
+
 	// enable device to generate interrupts
 	IPR1bits.RCIP = 1; // receive will be high priority
 	IPR1bits.TXIP = 0; // transmit will be low priority
 	PIE1bits.RCIE = 1;
-	PIE1bits.TXIE = 1; 
-	
+	PIE1bits.TXIE = 1;
+
 	// General interrupt control registers
-	INTCONbits.GIEH = 1; 
+	INTCONbits.GIEH = 1;
 	INTCONbits.GIEL = 1;
-	
 }
+
+#ifdef MY_I2C
+void IdleI2C(){
+	// Wait for Idle Status
+	while(SSPSTATbits.BF);
+}
+
+void StartI2C(){
+	//Send Start Condition
+	SSPCON2bits.SEN = 1;
+}
+
+void RestartI2C(){
+	SSPCON2bits.RSEN = 1;
+	while(SSPCON2bits.RSEN == 1);
+}
+
+void StopI2C(){
+	SSPCON2bits.PEN = 1;
+	while(SSPCON2bits.PEN == 1);
+}
+
+void WriteI2C( unsigned char data ){
+	SSPCON2bits.ACKSTAT = 1;
+	SSPBUF = data;
+	while(SSPCON2bits.ACKSTAT == 1);
+}
+void NotAckI2C(){
+	SSPCON2bits.ACKDT = 1; //NotAck
+	SSPCON2bits.ACKEN = 1; //Send NotAck
+}
+
+int getsI2C(unsigned char* data, unsigned char length){
+	int i;
+	for(i = 0; i < length; i++){
+		SSPCON2bits.RCEN = 1; //Do Recive
+		while(SSPCON2bits.RCEN == 1);
+		data[i] = SSPBUF;
+	}
+}
+
+unsigned char EEAckPolling(unsigned char data ){
+	StartI2C();
+	SSPCON2bits.ACKSTAT = 1;
+	SSPBUF = data;
+        IdleI2C();
+	return !SSPCON2bits.ACKSTAT;
+}
+#endif
 
 void HDByteWriteI2C( unsigned char ControlByte, unsigned char HighAdd, unsigned char LowAdd, unsigned char data )
 {
-  IdleI2C();                      // ensure module is idle
-  StartI2C();                     // initiate START condition
-  while ( SSPCON2bits.SEN );      // wait until start condition is over 
-  WriteI2C( ControlByte );        // write 1 byte - R/W bit should be 0
-  IdleI2C();                      // ensure module is idle
-  WriteI2C( HighAdd );            // write address byte to EEPROM
-  IdleI2C();                      // ensure module is idle
-  WriteI2C( LowAdd );             // write address byte to EEPROM
-  IdleI2C();                      // ensure module is idle
-  WriteI2C ( data );              // Write data byte to EEPROM
-  IdleI2C();                      // ensure module is idle
-  StopI2C();                      // send STOP condition
-  while ( SSPCON2bits.PEN );      // wait until stop condition is over 
-  while (EEAckPolling(ControlByte));  //Wait for write cycle to complete
+  //IdleI2C(); // ensure module is idle
+  StartI2C(); // initiate START condition
+  WriteI2C( ControlByte ); // write 1 byte - R/W bit should be 0
+  IdleI2C(); // ensure module is idle
+  WriteI2C( HighAdd ); // write address byte to EEPROM
+  IdleI2C(); // ensure module is idle
+  WriteI2C( LowAdd ); // write address byte to EEPROM
+  IdleI2C(); // ensure module is idle
+  WriteI2C ( data ); // Write data byte to EEPROM
+  IdleI2C(); // ensure module is idle
+  StopI2C(); // send STOP condition
+  while ( SSPCON2bits.PEN ); // wait until stop condition is over
+  while (EEAckPolling(ControlByte)); //Wait for write cycle to complete
 }
 
-void HDByteReadI2C( unsigned char ControlByte, unsigned char HighAdd, unsigned char LowAdd, unsigned char *data, unsigned char length )
+void HDByteReadI2C( unsigned char ControlByte, unsigned char HighAdd, unsigned char LowAdd, unsigned char* data, unsigned char length )
 {
-  IdleI2C();                      // ensure module is idle
-  StartI2C();                     // initiate START condition
-  while ( SSPCON2bits.SEN );      // wait until start condition is over 
-  WriteI2C( ControlByte );        // write 1 byte 
-  IdleI2C();                      // ensure module is idle
-  WriteI2C( HighAdd );            // WRITE word address to EEPROM
-  IdleI2C();                      // ensure module is idle
-  while ( SSPCON2bits.RSEN );     // wait until re-start condition is over 
-  WriteI2C( LowAdd );             // WRITE word address to EEPROM
-  IdleI2C();                      // ensure module is idle
-  RestartI2C();                   // generate I2C bus restart condition
-  while ( SSPCON2bits.RSEN );     // wait until re-start condition is over 
+  //IdleI2C(); // ensure module is idle
+  StartI2C(); // initiate START condition
+  while ( SSPCON2bits.SEN ); // wait until start condition is over
+  WriteI2C( ControlByte ); // write 1 byte
+  IdleI2C(); // ensure module is idle
+  WriteI2C( HighAdd ); // WRITE word address to EEPROM
+  IdleI2C(); // ensure module is idle
+  WriteI2C( LowAdd ); // WRITE word address to EEPROM
+  IdleI2C(); // ensure module is idle
+  RestartI2C(); // generate I2C bus restart condition
+  while ( SSPCON2bits.RSEN ); // wait until re-start condition is over
   WriteI2C( ControlByte | 0x01 ); // WRITE 1 byte - R/W bit should be 1 for read
-  IdleI2C();                      // ensure module is idle
-  getsI2C( data, length );       // read in multiple bytes
-  NotAckI2C();                    // send not ACK condition
-  while ( SSPCON2bits.ACKEN );    // wait until ACK sequence is over 
-  StopI2C();                      // send STOP condition
-  while ( SSPCON2bits.PEN );      // wait until stop condition is over 
+  IdleI2C(); // ensure module is idle
+  getsI2C( data, length ); // read in multiple bytes
+  NotAckI2C(); // send not ACK condition
+  //while ( SSPCON2bits.ACKEN ); // wait until ACK sequence is over
+  StopI2C(); // send STOP condition
+  while ( SSPCON2bits.PEN ); // wait until stop condition is over
 }
 
 
@@ -328,23 +342,33 @@ void main(void){
 	unsigned char i;
     setup();
 
-	HDByteReadI2C(0xa0, 0, 0, (unsigned char*)data, 2);
-	
-
+	HDByteReadI2C(0xa0, 0, 0, &counts, 4);
 	resetCursor();
-	insertChar(data[1]);
-	insertChar(data[0]);
-	counter = 10*(data[1]-0x30) + data[0]-0x30;
+        for(i = 0; i<4; i++){
+            if(counts[i] > 99){
+                counts[i] = 0;
+            }
+            insertChar((counts[i]/10) + 0x30);
+            insertChar((counts[i] - (counts[i]/10)*10) + 0x30);
+            insertChar(0x20);
+            insertChar(0x20);
+        }
+
+        counter = counts[0];
+
+
+        for(i = 0; i<3; i++){
+            HDByteWriteI2C(0xa0, 0, 0+1+i, counts[i]);
+        }
+
 
 	while (1){
 		while(PORTAbits.RA4 ==1);
 		waitms(20);
 		//T2CONbits.TMR2ON = 0;
-
 		//PORTBbits.RB3 = PORTBbits.RB3^1;
 		while (PORTAbits.RA4 == 0);
 		waitms(10);
 
 	}
-		
 }
